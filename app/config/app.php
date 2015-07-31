2b85068aaeec35bfca1b578a2a77144d0986520a
Zack Devine <zdevine@me.com>
2015-07-31 11:42:36 -0400

    Update app.php

--- app/config/app.php
+++ app/config/app.php
@@ -65,18 +65,18 @@ return array(
 	|
 	*/
 
-    'fallback_locale' => 'en',
+        'fallback_locale' => 'en',
 
-    /*
-    |-------------------------------------------------------------------------
-    | Logger-Specific Settings
-    |-------------------------------------------------------------------------
-    |
-    | These settings apply specifically only to the current application.
-    |
-    */
+        /*
+        |-------------------------------------------------------------------------
+        | Logger-Specific Settings
+        |-------------------------------------------------------------------------
+        |
+        | These settings apply specifically only to the current application.
+        |
+        */
 
-    'logger_appname' => 'My Application',
+        'logger_appname' => 'My Application',
 
 	/*
 	|--------------------------------------------------------------------------
