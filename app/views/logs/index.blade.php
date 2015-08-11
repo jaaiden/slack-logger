dc2ea2f91529098e44ef1db574923a870bafa708
Zack Devine <zdevine@me.com>
2015-08-10 20:58:06 -0400

    Updated main logs page

--- app/views/logs/index.blade.php
+++ app/views/logs/index.blade.php
@@ -5,7 +5,12 @@
 
       <div class="col-md-8">
         <h3 class="page-header">{{ Config::get('app.logger_appname') }} Logs</h3>
-        
+        <p class="lead">Currently logged channels:</p>
+        <ul class="list-unstyled">
+          @foreach (Config::get('app.logger_channels') as $logchannel)
+            <li>{{ $logchannel }}</li>
+          @endforeach
+        </ul>
       </div>
 
       <div class="col-md-4">
