0e9f59bc804b7fe941c30e94904d951a606b1a2b
Zack Devine <zdevine@me.com>
2015-08-10 20:30:27 -0400

    Updated SlackController.php

--- app/routes.php
+++ app/routes.php
@@ -25,4 +25,46 @@ Route::group(array('prefix' => 'logs'), function()
 });
 
 // POST Routes
-Route::post('log', array('uses' => 'SlackController@logMessage'));
+// Route::post('log', array('uses' => 'SlackController@logMessage'));
+Route::post('log', function()
+{
+  $channel = Input::get('channel_name');
+  $username = Input::get('user_name');
+  $message = Input::get('text');
+
+  // We'll log slackbot's messages, we just won't track them as a user statistic.
+  if ($username != "slackbot")
+  {
+    $user = Stats::where('username', '=', $username)->first();
+    if (is_null($user))
+    {
+      $user = new Stats;
+      $user->username = $username;
+      $user->msgcount = 1;
+      $user->wordcount = str_word_count($message);
+      $user->ratio = 0;
+      // $user->ratio = round(((str_word_count($message))/1), 2);
+      $user->save();
+    }else
+    {
+      $user->msgcount += 1;
+      $user->wordcount += str_word_count($message);
+      $user->ratio = 0;
+      // $user->ratio = round((($user->wordcount + str_word_count($message))/($user->msgcount + 1)), 2);
+      $user->save();
+    }
+
+    $log = new Logger;
+    $log->username = $username;
+    $log->message = $message;
+    $log->channel = $channel;
+    $log->save();
+  }else
+  {
+    $log = new Logger;
+    $log->username = "slackbot";
+    $log->message = $message;
+    $log->channel = $channel;
+    $log->save();
+  }
+});
